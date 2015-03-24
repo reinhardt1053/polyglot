b673f92f1521f860c56c26d8584406d6d95b8c92
Massimo Fazzolari <reinhardt1053@gmail.com>
2015-03-24 10:15:30 +0100

    Add Frame Tree

---
+++ diktor/Diktor/layout/frametree.cpp
@@ -0,0 +1,9 @@
+//
+//  builder.cpp
+//  Diktor
+//
+//  Created by Massimo Fazzolari on 23/03/15.
+//  Copyright (c) 2015 Massimo Fazzolari. All rights reserved.
+//
+
+#include "builder.h"
---
+++ diktor/Diktor/layout/frametree.h
@@ -0,0 +1,14 @@
+//
+//  builder.h
+//  Diktor
+//
+//  Created by Massimo Fazzolari on 23/03/15.
+//  Copyright (c) 2015 Massimo Fazzolari. All rights reserved.
+//
+
+#ifndef __Diktor__builder__
+#define __Diktor__builder__
+
+#include <stdio.h>
+
+#endif /* defined(__Diktor__builder__) */
